13cb3493cd4b11e6dfa5f57ce6857c0866772688
Segath <35260844+Segath@users.noreply.github.com>
2018-10-02 15:14:39 +0200

    Added delete and new errorhandlers
    Added delete endpoint.
    Changed returns to be on one line.
    Added errorhandler for uknown location and missing JSON's.

--- src/locations.py
+++ src/locations.py
@@ -7,6 +7,9 @@ bp = Blueprint('locations', __name__, url_prefix='/locations')
 
 @bp.route('<int:locId>/inventories', methods=['GET'])
 def get_all_items(locId):
+    if not Location.query.filter_by(id=locId).first():
+        return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
+
     location = Location.query.filter_by(id = locId).first()
     output = []
 
@@ -17,18 +20,19 @@ def get_all_items(locId):
         output.append(item_data)
 
     resp = jsonify({'items' : output})
-    resp.status_code = 200
 
-    return resp
+    return resp, 200
 
 
 @bp.route('<int:locId>/inventories/<string:itemName>', methods=['GET'])
 def get_amount_of_item(locId, itemName):
+    if not Location.query.filter_by(id=locId).first():
+        return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
+
     get_item = Item.query.filter_by(location_id = locId, name = itemName).first()
 
     if not get_item:
-        resp = jsonify({'code' : '404', 'message' : 'Item not in inventory', 'description' : 'This item is not located in this inventory'})
-        return resp, 404
+        return jsonify({'code' : '404', 'message' : 'Item not in inventory', 'description' : 'This item is not located in this inventory'}), 404
 
     resp = jsonify({'amount' : get_item.amount})
     return resp, 200
@@ -36,13 +40,18 @@ def get_amount_of_item(locId, itemName):
 
 @bp.route('<int:locId>/inventories/<string:itemName>', methods=['PUT'])
 def update_item_amount(locId, itemName):
+    if not Location.query.filter_by(id=locId).first():
+        return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
+
+    if not request.data:
+        return jsonify({'code' : '400', 'message' : 'No JSON','description' : 'Expected a JSON object'}), 400
     try:
         data = request.get_json()
     except (ValueError, KeyError, TypeError):
         resp = jsonify({'code' : '400', 'message' : 'Failed to decode','description' : 'Expected a JSON object'})
         return resp, 400
     
-    if not data['itemAmount']:
+    if not 'itemAmount' in data:
         return jsonify({'code' : '400', 'message' : 'Not enough data', 'description' : 'Missing item name or amount'})
     
     get_item = Item.query.filter_by(location_id = locId, name = itemName).first()
@@ -53,34 +62,52 @@ def update_item_amount(locId, itemName):
     get_item.amount = data['itemAmount']
     db.session.commit()
 
-    resp = jsonify({'code' : '204', 'message' : 'Item updated','description' : 'The item has successfully been updated'})
-    return resp,204
+    return jsonify({'code' : '204', 'message' : 'Item updated','description' : 'The item has successfully been updated'}),204
 
 
 @bp.route('<int:locId>/inventories', methods=['POST'])
 def create_new_item(locId):
+    if not Location.query.filter_by(id=locId).first():
+        return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
+
+    if not request.data:
+        return jsonify({'code' : '400', 'message' : 'No JSON','description' : 'Expected a JSON object'}), 400
+
+    if not request.content_type == 'application/json':
+        return jsonify({'code' : '400', 'message' : 'Expected JSON','description' : 'POST method expected a JSON in application/json'}), 400
+
     try:
         data = request.get_json()
     except (ValueError, KeyError, TypeError):
-        return jsonify({'code' : '400', 'message' : 'Failed to decode','description' : 'Expected a JSON object'})
+        return jsonify({'code' : '400', 'message' : 'Failed to decode','description' : 'Expected a JSON object'}), 400
 
 
-    if not data['itemName'] or not data['itemAmount']:
-        return jsonify({'code' : '400', 'message' : 'Not enough data', 'description' : 'Missing item name or amount'})
+    if not 'itemName' in data or not 'itemAmount' in data:
+        return jsonify({'code' : '400', 'message' : 'Not enough data', 'description' : 'Missing item name or amount'}), 400
 
 
     location = Location.query.filter_by(id=locId).first()
     for item in location.items:
         if data['itemName'] == item.name:
-            return jsonify({'code' : '1234', 'message' : 'New item not created', 'description' : 'The requested item is already in the database'})
+            return jsonify({'code' : '304', 'message' : 'New item not created', 'description' : 'The requested item is already in the database'}), 304
 
 
     new_item = Item(name=data['itemName'], amount=data['itemAmount'], location_id = locId)
     db.session.add(new_item)
     db.session.commit()
-    return jsonify({'code' : '201', 'message' : 'New item created', 'description' : 'An item has successfully been created'})
+    return jsonify({'code' : '201', 'message' : 'New item created', 'description' : 'An item has successfully been created'}), 201
 
 
 @bp.route('<int:locId>/inventories/<string:itemName>', methods=['DELETE'])
-def delete_item():
-    return ''
+def delete_item(locId, itemName):
+    if not Location.query.filter_by(id=locId).first():
+        return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
+
+    get_item = Item.query.filter_by(location_id = locId, name = itemName).first()
+
+    if not get_item:
+        return jsonify({'code' : '404', 'message' : 'Item not in inventory', 'description' : 'This item is not located in this inventory'}), 404
+    else:
+        db.session.delete(get_item)
+        db.session.commit()
+        return jsonify({'code' : '204', 'message' : 'No Content', 'description' : 'An item has successfully been deleted'}), 204
