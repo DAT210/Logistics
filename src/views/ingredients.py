0ee2f6ff32b9e418c9f6b3c84649690d9e5405f0
Segath <35260844+Segath@users.noreply.github.com>
2018-10-23 15:41:25 +0200

    More name changes and implement database script
    Implementet database script to tests.
    Changed references to use the new database schema.
    Changed docker port.

--- src/views/ingredients.py
+++ src/views/ingredients.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify, Blueprint
 from flask_sqlalchemy import SQLAlchemy
-from ..models import db, Item, Location
+from ..models import db, Ingredient, Location
 from .locations import user, token_required
 
 bp = Blueprint('ingredients', __name__, url_prefix='/v1/locations/')
 
 
 
-# Route to get every item in the inventory of the specified location
+# Route to get every ingredient in the inventory of the specified location
 @bp.route('<int:locId>/ingredients', methods=['GET'])
 @token_required
-def get_all_items(current_user, locId):
+def get_all_ingredients(current_user, locId):
     # Check if location exists in the database, this test is in every route
     if not Location.query.filter_by(id=locId).first():
         return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
@@ -18,34 +18,34 @@ def get_all_items(current_user, locId):
     location = Location.query.filter_by(id = locId).first()
     output = []
 
-    for item in location.items:
-        item_data = {}
-        item_data['name'] = item.name
-        item_data['amount'] = item.amount
-        output.append(item_data)
+    for ingredient in location.ingredients:
+        ingredient_data = {}
+        ingredient_data['name'] = ingredient.name
+        ingredient_data['amount'] = ingredient.amount
+        output.append(ingredient_data)
 
-    return jsonify({'items' : output}), 200
+    return jsonify({'ingredients' : output}), 200
 
 
-# Route to get a specific item in the inventory of a location
-@bp.route('<int:locId>/ingredients/<string:itemName>', methods=['GET'])
+# Route to get a specific ingredient in the inventory of a location
+@bp.route('<int:locId>/ingredients/<string:ingredientName>', methods=['GET'])
 @token_required
-def get_amount_of_item(current_user, locId, itemName):
+def get_amount_of_ingredient(current_user, locId, ingredientName):
     if not Location.query.filter_by(id=locId).first():
         return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
 
-    get_item = Item.query.filter_by(location_id = locId, name = itemName).first()
+    get_ingredient = Ingredient.query.filter_by(location_id = locId, name = ingredientName).first()
 
-    if not get_item:
-        return jsonify({'code' : '404', 'message' : 'Item not in inventory', 'description' : 'This item is not located in this inventory'}), 404
+    if not get_ingredient:
+        return jsonify({'code' : '404', 'message' : 'ingredient not in inventory', 'description' : 'This ingredient is not located in this inventory'}), 404
 
-    return jsonify({'amount' : get_item.amount}), 200
+    return jsonify({'amount' : get_ingredient.amount}), 200
 
 
-# Route to update a item by adding or subtracting the amount
-@bp.route('<int:locId>/ingredients/<string:itemName>', methods=['PUT'])
+# Route to update a ingredient by adding or subtracting the amount
+@bp.route('<int:locId>/ingredients/<string:ingredientName>', methods=['PUT'])
 @token_required
-def update_item_amount(current_user, locId, itemName):
+def update_ingredient_amount(current_user, locId, ingredientName):
     if not Location.query.filter_by(id=locId).first():
         return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
     
@@ -60,39 +60,39 @@ def update_item_amount(current_user, locId, itemName):
         return jsonify({'code' : '400', 'message' : 'Failed to decode','description' : 'Expected a JSON object'}), 400
     
     # Check if the names are correct in the json input, and if they exist
-    if not 'itemAmount' in data or not 'action' in data:
+    if not 'ingredientAmount' in data or not 'action' in data:
         return jsonify({'code' : '400', 'message' : 'Not enough data', 'description' : 'Missing amount or action'}), 400
     
-    # Check if itemAmount is a int
-    if not isinstance(data['itemAmount'], int):
+    # Check if ingredientAmount is a int
+    if not isinstance(data['ingredientAmount'], int):
         return jsonify({'code' : '400', 'message' : 'Bad input', 'description' : 'Amount needs to be an integer'}), 400
 
-    get_item = Item.query.filter_by(location_id = locId, name = itemName).first()
-    # Check if the item exists in the inventory
-    if not get_item:
-        return jsonify({'code' : '404', 'message' : 'Item not in inventory', 'description' : 'This item is not located in this inventory'}), 404
+    get_ingredient = Ingredient.query.filter_by(location_id = locId, name = ingredientName).first()
+    # Check if the ingredient exists in the inventory
+    if not get_ingredient:
+        return jsonify({'code' : '404', 'message' : 'ingredient not in inventory', 'description' : 'This ingredient is not located in this inventory'}), 404
     
     if data['action'] == 'add':
-        get_item.amount = get_item.amount + data['itemAmount']
+        get_ingredient.amount = get_ingredient.amount + data['ingredientAmount']
 
     elif data['action'] == 'subtract':
-        amount = get_item.amount - data['itemAmount']
+        amount = get_ingredient.amount - data['ingredientAmount']
         if amount < 0:
             amount = 0
-        get_item.amount = amount
+        get_ingredient.amount = amount
     
     else:
         return jsonify({'code' : '400', 'message' : 'Invalid action', 'description' : 'Uknown action in input'}), 400
 
     db.session.commit()
 
-    return jsonify({'code' : '204', 'message' : 'Item updated','description' : 'The item has successfully been updated'}),204
+    return jsonify({'code' : '204', 'message' : 'ingredient updated','description' : 'The ingredient has successfully been updated'}),204
 
 
-# Route to add new item to the inventory
+# Route to add new ingredient to the inventory
 @bp.route('<int:locId>/ingredients', methods=['POST'])
 @token_required
-def create_new_item(current_user, locId):
+def create_new_ingredient(current_user, locId):
     if not Location.query.filter_by(id=locId).first():
         return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
 
@@ -108,36 +108,36 @@ def create_new_item(current_user, locId):
         return jsonify({'code' : '400', 'message' : 'Failed to decode','description' : 'Expected a JSON object'}), 400
 
 
-    if not 'itemName' in data or not 'itemAmount' in data:
-        return jsonify({'code' : '400', 'message' : 'Not enough data', 'description' : 'Missing item name or amount'}), 400
+    if not 'ingredientName' in data or not 'ingredientAmount' in data:
+        return jsonify({'code' : '400', 'message' : 'Not enough data', 'description' : 'Missing ingredient name or amount'}), 400
 
-    if not isinstance(data['itemAmount'], int):
+    if not isinstance(data['ingredientAmount'], int):
         return jsonify({'code' : '400', 'message' : 'Bad input', 'description' : 'Amount needs to be an integer'}), 400
 
     location = Location.query.filter_by(id=locId).first()
-    for item in location.items:
-        if data['itemName'] == item.name:
-            return jsonify({'code' : '304', 'message' : 'New item not created', 'description' : 'The requested item is already in the database'}), 304
+    for ingredient in location.ingredients:
+        if data['ingredientName'] == ingredient.name:
+            return jsonify({'code' : '304', 'message' : 'New ingredient not created', 'description' : 'The requested ingredient is already in the database'}), 304
 
 
-    new_item = Item(name=data['itemName'], amount=data['itemAmount'], location_id = locId)
-    db.session.add(new_item)
+    new_ingredient = Ingredient(name=data['ingredientName'], amount=data['ingredientAmount'], location_id = locId)
+    db.session.add(new_ingredient)
     db.session.commit()
-    return jsonify({'code' : '201', 'message' : 'New item created', 'description' : 'An item has successfully been created'}), 201
+    return jsonify({'code' : '201', 'message' : 'New ingredient created', 'description' : 'An ingredient has successfully been created'}), 201
 
 
-# Route to delete a item from the inventory
-@bp.route('<int:locId>/ingredients/<string:itemName>', methods=['DELETE'])
+# Route to delete a ingredient from the inventory
+@bp.route('<int:locId>/ingredients/<string:ingredientName>', methods=['DELETE'])
 @token_required
-def delete_item(current_user, locId, itemName):
+def delete_ingredient(current_user, locId, ingredientName):
     if not Location.query.filter_by(id=locId).first():
         return jsonify({'code' : '404', 'message' : 'Location does not exist', 'description' : 'This location does not exist in the database'}), 404
 
-    get_item = Item.query.filter_by(location_id = locId, name = itemName).first()
+    get_ingredient = Ingredient.query.filter_by(location_id = locId, name = ingredientName).first()
 
-    if not get_item:
-        return jsonify({'code' : '404', 'message' : 'Item not in inventory', 'description' : 'This item is not located in this inventory'}), 404
+    if not get_ingredient:
+        return jsonify({'code' : '404', 'message' : 'ingredient not in inventory', 'description' : 'This ingredient is not located in this inventory'}), 404
     else:
-        db.session.delete(get_item)
+        db.session.delete(get_ingredient)
         db.session.commit()
-        return jsonify({'code' : '204', 'message' : 'No Content', 'description' : 'An item has successfully been deleted'}), 204
+        return jsonify({'code' : '204', 'message' : 'No Content', 'description' : 'An ingredient has successfully been deleted'}), 204
